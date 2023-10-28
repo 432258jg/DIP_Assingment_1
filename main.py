ef9d77f3b748c92e9772e3d46d498f652d2a9c71
Jelle <62745439+432258jg@users.noreply.github.com>
2023-10-28 19:28:01 +0200

    Added picture, cascade.xml and .py

--- main.py
+++ main.py
@@ -1,73 +1,37 @@
 import cv2
-import numpy as np
+#
+# Reading the image
+img = cv2.imread('Pictures/Test/Test2.jpg')
 
-"""gamma
-def gammaCorrection(src, gamma):
-    invGamma = 1 / gamma
+# Converting image to grayscale
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    table = [((i / 255) ** invGamma) * 255 for i in range(256)]
-    table = np.array(table, np.uint8)
+# Loading the required haar-cascade xml classifier file
+haar_cascade = cv2.CascadeClassifier('Pictures/classifier/cascade.xml')
 
-    return cv2.LUT(src, table)
+# Applying the face detection method on the grayscale image
+faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9)
+
+# Iterating through rectangles of detected faces
+for (x, y, w, h) in faces_rect:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow('Detected faces', img)
+
+cv2.waitKey(0)
 
 
-img = cv2.imread('test.jpg')
-img_output = gammaCorrection(img, 1.2)
-
-compare = np.concatenate((img, img_output), axis=1)
-
-cv2.imshow('Original image', img)
-cv2.imshow('Gamma corrected image', img_output)
+#here is code to check the output
+import cv2
+face_cascade=cv2.CascadeClassifier(r"F:\data imp\coding\Opencv\data_Downlode\data of cats\classifier\cascade.xml")###path of cascade file
+## following is an test image u can take any image from the p folder in the temp folder and paste address of it on below line
+img= cv2.imread(r"F:\data imp\coding\Opencv\data_Downlode\data of cats\p\1588088937587.jpg")###path of image file which we want to detect
+resized = cv2.resize(img,(400,200))
+gray=cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
+faces=face_cascade.detectMultiScale(gray,6.5,17)#try to tune this 6.5 and 17 parameter to get good result
+##if not getting good result try to train new cascade.xml file again deleting other file expect p and n in temp folder
+for(x,y,w,h) in faces:
+    resized=cv2.rectangle(resized,(x,y),(x+w,y+h),(0,255,0),2)
+cv2.imshow('img',resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-"""
-"""Histogram equalization
-img = cv2.imread('test3.jpg')
-img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-
-img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-
-compare = np.concatenate((img, img_output), axis=1)
-
-cv2.imshow('Original image', img)
-cv2.imshow('Output image', img_output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
-"""Gaussian filter
-img = cv2.imread('test3.jpg')
-
-img_output = cv2.GaussianBlur(img, (5,5), cv2.BORDER_DEFAULT)
-# img_output = cv2.GaussianBlur(img,(9,9),cv2.BORDER_REFLECT_101)
-
-compare = np.concatenate((img, img_output), axis=1)
-
-cv2.imshow('Original image', img)
-cv2.imshow('Output image', img_output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
-"""Median filter"""
-img = cv2.imread('test3.jpg')
-
-img_output = cv2.medianBlur(img, 5)
-#laplacian = cv2.Laplacian(img,cv2.CV_64F)
-compare = np.concatenate((img, img_output), axis=1)
-
-#cv2.imshow('Original image', img)
-cv2.imshow('Output image', compare)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-"""Laplacian filter
-img = cv2.imread('test3.jpg')
-
-#img_output = cv2.medianBlur(img, 5)
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
-compare = np.concatenate((img, laplacian), axis=1)
-
-#cv2.imshow('Original image', img)
-cv2.imshow('Output image', compare)
-cv2.waitKey(0)
-cv2.destroyAllWindows()"""
